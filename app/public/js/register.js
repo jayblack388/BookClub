3995fe0f25929aee90288ea3ec1cea181696de80
jayblack388 <jblackwell072588@gmail.com>
2018-02-19 14:55:56 -0500

    created starter folders/files and register/landing page

---
+++ app/public/js/register.js
@@ -0,0 +1,13 @@
+$(document).ready(()=>{
+
+    $("#hideLogin").click(()=>{
+        $("#loginForm").hide();
+        $("#registerForm").show();
+    });
+
+    $("#hideRegister").click(()=>{
+        $("#loginForm").show();
+        $("#registerForm").hide();
+    });
+
+});
