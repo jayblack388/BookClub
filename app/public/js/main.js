3a3f01278a1d4d4dcff852671d951d797672195e
jayblack388 <jblackwell072588@gmail.com>
2018-02-22 12:47:54 -0500

    saving front branch and changing footer cards

---
+++ app/public/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(()=>{
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $("#collapseOne").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $("#collapseTwo").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $("#collapseThree").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    $("#collapseFour").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+})
